ee42e7359972c4dd9d4ed904307c7fe48ffaeb44
William Langdon <williamlangdon78@gmail.com>
2025-03-11 15:51:26 -0500

    character counting

--- newdalloway/src/main/java/com/billybot3000/Main.java
+++ newdalloway/src/main/java/com/billybot3000/Main.java
@@ -2,11 +2,8 @@ package com.billybot3000;
 
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
-import opennlp.tools.util.Span;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -43,11 +40,11 @@ public class Main {
     private static String[] tokenize() {
         TokenizerME tokenizer = null;
         try {
-            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/models/opennlp-en-ud-ewt-token-1.2-2.5.0.bin");
+            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/models/opennlp-en-ud-ewt-tokens-1.2-2.5.0.bin");
             tokenizer = new TokenizerME(new TokenizerModel(file));
             file.close();
         } catch(IOException e) {
-            System.out.println("error lmao");
+            System.out.println("error lmao 3");
             System.exit(1);
         }
         return tokenizer.tokenize(getText("newdalloway/src/main/java/com/billybot3000/dalloway.txt"));
@@ -75,29 +72,55 @@ public class Main {
             }
             bw.close();
         } catch (Exception e) {
-            System.out.println("error lmao");
+            System.out.println("error lmao 2");
             System.exit(1);
         }
     }
     
     private static void nameAnalysis() {
-        NameFinderME nameFinder = null;
-        try {
-            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/models/en-ner-person.bin");
-            nameFinder = new NameFinderME(new TokenNameFinderModel(file));
-            file.close();
-        } catch(IOException e) {
-            System.out.println("error lmao");
-            System.exit(1);
-        }
         String[] tokens = tokenize();
-        Span[] names = nameFinder.find(tokens);
-        ArrayList<String> nameList = new ArrayList<String>();
-        for (Span name : names) {
-            for (int i = name.getStart(); i < name.getEnd(); i++) {
-                nameList.add(tokens[name.getStart() + i]);
-                System.out.println(tokens[name.getStart() + i]);
+        
+        // name map
+        ArrayList<Integer> clarissa = new ArrayList<Integer>();
+        ArrayList<Integer> peter = new ArrayList<Integer>();
+        ArrayList<Integer> septimus = new ArrayList<Integer>();
+        ArrayList<Integer> lucrezia = new ArrayList<Integer>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("Clarissa") || tokens[i].equals("Mrs. Dalloway")) {
+                clarissa.add(i);
+            } else if (tokens[i].equals("Peter") || tokens[i].equals("Walsh")) {
+                peter.add(i);
+            } else if (tokens[i].equals("Septimus")) {
+                septimus.add(i);
+            } else if (tokens[i].equals("Lucrezia") || tokens[i].equals("Rezia")) {
+                lucrezia.add(i);
             }
+        }
+        // write data to csv
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("newdalloway/src/main/java/com/billybot3000/output/characters.csv"));
+            bw.write("Clarissa, ");
+            for (int i = 0; i < clarissa.size(); i++) {
+                bw.write(clarissa.get(i) + ",");
+            }
+            bw.write("\nPeter, ");
+            for (int i = 0; i < peter.size(); i++) {
+                bw.write(peter.get(i) + ",");
+            }
+            bw.write("\nSeptimus, ");
+            for (int i = 0; i < septimus.size(); i++) {
+                bw.write(septimus.get(i) + ",");
+            }
+            bw.write("\nLucrezia, ");
+            for (int i = 0; i < lucrezia.size(); i++) {
+                bw.write(lucrezia.get(i) + ",");
+            }
+            bw.close();
+        } catch (Exception e) {
+            System.out.println("error lmao 2");
+            System.exit(1);
         }
     }
     
