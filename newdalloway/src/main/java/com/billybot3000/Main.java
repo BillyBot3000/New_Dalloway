b0ffa10e2d966fddaedc3ceb10a26cf8f856396b
William Langdon <williamlangdon78@gmail.com>
2025-03-10 21:02:42 -0500

    temporary stopping point

--- newdalloway/src/main/java/com/billybot3000/Main.java
+++ newdalloway/src/main/java/com/billybot3000/Main.java
@@ -2,6 +2,8 @@ package com.billybot3000;
 
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,35 +16,41 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+        sentenceDetection();
+    }
+
+    private static String getText(String filename) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found lmao");
+            System.exit(1);
+        }
+        String text = "";
+        while(scanner.hasNextLine()) {
+            text += scanner.nextLine();
+        }
+        scanner.close();
+        return text;
+    }
+
+    private static void sentenceDetection() {
         SentenceDetectorME sentenceDetector = null;
         try {
-            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/opennlp-en-ud-ewt-sentence-1.2-2.5.0.bin");
+            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/models/opennlp-en-ud-ewt-sentence-1.2-2.5.0.bin");
             sentenceDetector = new SentenceDetectorME(new SentenceModel(file));
             file.close();
         } catch(IOException e) {
             e.printStackTrace();
         }
-
-        // scanner
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File("newdalloway/src/main/java/com/billybot3000/dalloway.txt"));
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found lmao");
-            System.exit(1);
-        }
-        
         // detect sentences
-        String text = "";
-        while(scanner.hasNextLine()) {
-            text += scanner.nextLine();
-        }
-        String[] sentences = sentenceDetector.sentDetect(text);
+        String[] sentences = sentenceDetector.sentDetect(getText("newdalloway/src/main/java/com/billybot3000/dalloway.txt"));
 
         // write data to csv
         BufferedWriter bw = null;
         try {
-            bw = new BufferedWriter(new FileWriter("newdalloway/src/main/java/com/billybot3000/sentenceLength.csv"));
+            bw = new BufferedWriter(new FileWriter("newdalloway/src/main/java/com/billybot3000/output/sentenceLength.csv"));
             bw.write("Index,Character Count (including spaces),Word Count\n");
             for (int i = 0; i < sentences.length; i++) {
                 bw.write(i + "," + sentences[i].length() + "," + sentences[i].split(" ").length + "\n");
@@ -52,7 +60,18 @@ public class Main {
             System.out.println("error lmao");
             System.exit(1);
         }
-        
+    }
+    
+    private static void nameAnalysis() {
+        NameFinderME nameFinder = null;
+        try {
+            FileInputStream file = new FileInputStream("newdalloway/src/main/java/com/billybot3000/models/en-ner-person.bin");
+            nameFinder = new NameFinderME(new TokenNameFinderModel(file));
+            file.close();
+        } catch(IOException e) {
+            System.out.println("error lmao");
+            System.exit(1);
+        }
     }
     
 }
